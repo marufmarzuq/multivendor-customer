1f93a8ec787f7c11f8d771c6ad890a3f0e0e667d
atik65 <atikhasan801@gmail.com>
2022-07-28 02:10:01 +0600

    purchase history

--- src/components/vendor/purchaseHistory/PurchseHis.jsx
+++ src/components/vendor/purchaseHistory/PurchseHis.jsx
@@ -1,7 +1,14 @@
 import React, { Fragment } from "react";
 import purchaseStyle from "./purchaseHis.module.css";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { AiOutlineEye } from "react-icons/ai";
+import { BsArrowDown, BsDownload, BsEyeFill } from "react-icons/bs";
+
+import { useState } from "react";
+import Modal from "../orderModal/Modal";
 
 const PurchseHis = () => {
+  const [modal, setModal] = useState(false);
   return (
     <Fragment>
       <div className={`${purchaseStyle.background}`}>
@@ -11,17 +18,27 @@ const PurchseHis = () => {
         </section>
 
         <section>
-          <div className="row px-0 mx-0 ps-3 mt-4 pt-2">
-            <div className="col-4 col-lg-2"> Code </div>
-            <div className={`col-4 col-lg-2 ${purchaseStyle.hide}`}> Date </div>
-            <div className="col-4 col-lg-2"> Amount </div>
-            <div className={`col-4 col-lg-2 ${purchaseStyle.hide}`}>
-              Delivery Status
+          <div
+            className={`row px-0 mx-0 ps-3 mt-4 pt-2 ${purchaseStyle.header}`}
+          >
+            <div className="col-4 col-lg-2">
+              <p>Code</p>
             </div>
             <div className={`col-4 col-lg-2 ${purchaseStyle.hide}`}>
-              Payment Status
+              <p>Date</p>
             </div>
-            <div className="col-4 col-lg-2"> Options </div>
+            <div className="col-4 col-lg-2">
+              <p>Amount</p>
+            </div>
+            <div className={`col-4 col-lg-2 ${purchaseStyle.hide}`}>
+              <p>Delivery Status</p>
+            </div>
+            <div className={`col-4 col-lg-2 ${purchaseStyle.hide}`}>
+              <p> Payment Status</p>
+            </div>
+            <div className="col-4 col-lg-2">
+              <p>Options</p>
+            </div>
           </div>
           <hr />
         </section>
@@ -34,14 +51,66 @@ const PurchseHis = () => {
                 alert("Details will appear in modal... Coming soon")
               }
             >
-              20210518
+              <p className={purchaseStyle.code}>20210518-44545</p>
             </div>
-            <div className="col-4 col-lg-2"> 18-05-2021 </div>
-            <div className="col-4 col-lg-2"> ৳1,000.000 </div>
-            <div className="col-4 col-lg-2"> Pending </div>
-            <div className="col-4 col-lg-2"> Unpaid </div>
             <div className="col-4 col-lg-2">
-              <button>del</button> <button>view</button> <button>down</button>
+              <p>18-05-2021 </p>
+            </div>
+            <div className="col-4 col-lg-2">
+              <p> ৳1,000.000 </p>
+            </div>
+            <div className="col-4 col-lg-2">
+              <span> On delivery</span>
+            </div>
+            <div className="col-4 col-lg-2">
+              <span className={purchaseStyle.paid}>Paid</span>
+            </div>
+            <div className="col-4 col-lg-2">
+              <button className={purchaseStyle.del}>
+                <RiDeleteBin2Line />
+              </button>
+              <button className={purchaseStyle.preview}>
+                <BsEyeFill />
+              </button>
+              <button className={purchaseStyle.invoice}>
+                <BsDownload />
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="row px-0 mx-0 ps-3 mt-4 pt-2">
+            <div
+              className="col-4 col-lg-2"
+              onClick={() =>
+                alert("Details will appear in modal... Coming soon")
+              }
+            >
+              <p className={purchaseStyle.code}>20210518-44545</p>
+            </div>
+            <div className="col-4 col-lg-2">
+              <p>18-05-2021 </p>
+            </div>
+            <div className="col-4 col-lg-2">
+              <p> ৳1,2000.000 </p>
+            </div>
+            <div className="col-4 col-lg-2">
+              <span>Pending</span>
+            </div>
+            <div className="col-4 col-lg-2">
+              <span className={purchaseStyle.unpaid}>UnPaid</span>
+            </div>
+            <div className="col-4 col-lg-2">
+              <button className={purchaseStyle.del}>
+                <RiDeleteBin2Line />
+              </button>
+              <button className={purchaseStyle.preview}>
+                <BsEyeFill />
+              </button>
+              <button className={purchaseStyle.invoice}>
+                <BsDownload />
+              </button>
             </div>
           </div>
         </section>
