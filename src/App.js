54a04a8a9feaebf8b30512324577bfce452eec4f
atik65 <atikhasan801@gmail.com>
2022-07-31 11:55:32 +0600

    language issues fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 //translation
-import {I18nextProvider} from 'react-i18next';
-import i18next from './translations/i18next';
+import { I18nextProvider } from "react-i18next";
+import i18next from "./translations/i18next";
 // route
 import { BrowserRouter } from "react-router-dom";
 import NavRoutes from "./routes_&_Navigations/routes/NavRoutes";
@@ -9,11 +9,11 @@ import NavRoutes from "./routes_&_Navigations/routes/NavRoutes";
 function App() {
   return (
     <Fragment>
-				<I18nextProvider i18n={i18next}>
-					<BrowserRouter>
-							<NavRoutes />
-						</BrowserRouter>
-				</I18nextProvider>
+      {/* <I18nextProvider i18n={i18next}> */}
+      <BrowserRouter>
+        <NavRoutes />
+      </BrowserRouter>
+      {/* </I18nextProvider> */}
     </Fragment>
   );
 }
--- src/translations/i18next.js
+++ src/translations/i18next.js
@@ -1,31 +1,22 @@
-import i18next from 'i18next';
-import common_fr from './fr/common.json';
-import common_en from './en/common.json';
-import common_ro from './ro/common.json';
-import common_es from './es/common.json';
-import common_it from './it/common.json';
+import i18next from "i18next";
+import common_fr from "./fr/common.json";
+import common_en from "./en/common.json";
+import common_ro from "./ro/common.json";
+import common_es from "./es/common.json";
+import common_it from "./it/common.json";
+import { initReactI18next } from "react-i18next";
 
 // languages
-i18next.init({
-    interpolation: { escapeValue: false },  // React already does escaping
-    lng:  'en', // language to use
-    resources: {
-        en: {
-            common: common_en // 'common' is our custom namespace
-        },
-        fr: {
-            common: common_fr
-        },
-        ro: {
-            common: common_ro
-        },
-        es: {
-            common: common_es
-        },
-        it: {
-            common: common_it
-        },
-    },
+i18next.use(initReactI18next).init({
+  interpolation: { escapeValue: false }, // React already does escaping
+  lng: localStorage.getItem("lang") || "en", // language to use
+  resources: {
+    en: { translation: common_en.common },
+    fr: { translation: common_fr.common },
+    ro: { translation: common_ro.common },
+    es: { translation: common_es.common },
+    it: { translation: common_it.common },
+  },
 });
 
-export default i18next
+export default i18next;
