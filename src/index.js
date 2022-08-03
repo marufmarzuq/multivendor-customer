5d42ca3e1dd009fa6b5093e2c355893a326e301d
nitu-xs <62047110+nitu-xs@users.noreply.github.com>
2022-08-03 08:09:48 +0600

    test

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 // ReactDOM.render(<App />, document.getElementById("root"));
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
-// test commit
-// test commit1
-// test commit3
+
 root.render(
   <React.StrictMode>
     <App />
