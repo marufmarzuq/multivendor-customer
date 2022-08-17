baf114a32471af98dae5a3ac95a8ddc1fd1df175
atik65 <atikhasan801@gmail.com>
2022-08-18 03:34:12 +0600

    shop setting

--- src/pages/seller/shopSetting/ShopSetting.jsx
+++ src/pages/seller/shopSetting/ShopSetting.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
 import SettingBanner from "./componetns/settingBanner/SettingBanner";
 import SettingInfo from "./componetns/settingInfo/SettingInfo";
 import SettingSocialLinks from "./componetns/settingSocialLinks/SettingSocialLinks";
 import settingStyle from "./shopSetting.module.css";
+
 const ShopSetting = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 10 });
+  }, []);
   return (
     <div className="add-product mx-3 mb-5">
       <div className=" d-flex justify-content-between me-md-5 pe-md-4 me-0 pe-0 mt-3 mb-3">
