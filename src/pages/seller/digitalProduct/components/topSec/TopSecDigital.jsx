03bb2ae2c1dc9b688ad8a7746d5ad2110ff854ce
atik65 <atikhasan801@gmail.com>
2022-08-20 16:13:26 +0600

    table header fix

--- src/pages/seller/digitalProduct/components/topSec/TopSecDigital.jsx
+++ src/pages/seller/digitalProduct/components/topSec/TopSecDigital.jsx
@@ -5,9 +5,8 @@ import { NavLink } from "react-router-dom";
 const TopSecDigital = () => {
   return (
     <div>
-      <h5>Digital Products</h5>
-
       <div className={topSecStyle.addBtnParent}>
+        <h5>Digital Products</h5>
         <NavLink className="btn btn-outline-success" to="/products/digital/add">
           Add New Digital Product
         </NavLink>
--- src/pages/seller/products/components/topSec/TopSec.jsx
+++ src/pages/seller/products/components/topSec/TopSec.jsx
@@ -5,9 +5,10 @@ import { Link } from "react-router-dom";
 const TopSec = () => {
   return (
     <div>
-      <h5>Products</h5>
+      {/* <h5 className={topSecStyle.head}>Products</h5> */}
 
       <div className={topSecStyle.addBtnParent}>
+        <h5>Products</h5>
         <Link className="btn btn-outline-success" to="/products/add">
           Add New Product
         </Link>
