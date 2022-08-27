61be5cb031bb68cee7b866643675a3bf2b9b2d97
nitu-xs <62047110+nitu-xs@users.noreply.github.com>
2022-08-27 11:08:51 +0600

    store added

--- src/pages/customer/stores/components/storesCom/StoreList.jsx
+++ src/pages/customer/stores/components/storesCom/StoreList.jsx
@@ -2,7 +2,45 @@ import React from "react";
 
 const StoreList = () => {
   return (
-			'StoreList'
+			<div className="wrapper">
+				<div className="container">
+					<div className="d-flex mb-5">
+						<div className="card">
+							<img className="card-img-top" src="..." alt="Card image cap"/>
+							<div className="card-body">
+								<h5 className="card-title">Store Name</h5>
+								<p className="d-flex"><span>rating</span>( 200 customers review)</p>
+								<p className="card-text d-flex"><span>location icon .</span>Address</p>
+								<p className="card-text d-flex"><span>phone icon .</span> 0144444444</p>
+								<p className="card-text d-flex"><span>email icon .</span> test@gmail.com</p>
+								<a href="#" className="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+						<div className="card">
+							<img className="card-img-top" src="..." alt="Card image cap"/>
+							<div className="card-body">
+								<h5 className="card-title">Store Name</h5>
+								<p className="d-flex"><span>rating</span>( 200 customers review)</p>
+								<p className="card-text d-flex"><span>location icon .</span>Address</p>
+								<p className="card-text d-flex"><span>phone icon .</span> 0144444444</p>
+								<p className="card-text d-flex"><span>email icon .</span> test@gmail.com</p>
+								<a href="#" className="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+						<div className="card">
+							<img className="card-img-top" src="..." alt="Card image cap"/>
+							<div className="card-body">
+								<h5 className="card-title">Store Name</h5>
+								<p className="d-flex"><span>rating</span>( 200 customers review)</p>
+								<p className="card-text d-flex"><span>location icon .</span>Address</p>
+								<p className="card-text d-flex"><span>phone icon .</span> 0144444444</p>
+								<p className="card-text d-flex"><span>email icon .</span> test@gmail.com</p>
+								<a href="#" className="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
   );
 };
 
