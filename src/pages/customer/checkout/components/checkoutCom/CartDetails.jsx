fb6f934c2a8675ed43fd445253f2404e1f924419
nitu-xs <42024930+nitu456@users.noreply.github.com>
2022-08-28 21:37:06 +0600

    checkout page

--- src/pages/customer/checkout/components/checkoutCom/CartDetails.jsx
+++ src/pages/customer/checkout/components/checkoutCom/CartDetails.jsx
@@ -1,6 +1,20 @@
+import CartTotal from "../../../cart/components/cartCom/CartTotal";
+
 const CartDetails = () => {
   return (
-			'CartDetails'
+  <div>
+		<h3>Cart Total</h3>
+		<div className="marketplace-products">
+			<div className="">
+				<div><img src="#"/><span>Store Name</span></div>
+				<CartTotal/>
+			</div>
+			<div className="">
+				<div><img src="#"/><span>Store Name</span></div>
+				<CartTotal/>
+			</div>
+		</div>
+  </div>
   );
 };
 
