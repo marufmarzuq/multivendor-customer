3a3a5481bae0fe41d7787b69626838dd89cf9ed8
atik65 <atikhasan801@gmail.com>
2022-08-28 21:05:09 +0600

    checkout page

--- src/pages/customer/checkout/Checkout.jsx
+++ src/pages/customer/checkout/Checkout.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import CheckoutForm from "./components/checkoutCom/CheckoutForm";
 import CartDetails from "./components/checkoutCom/CartDetails";
-
+import checkoutStyle from "./checkoutStyle.module.css";
+import BreadCumb from "../../../common/breadcumb/BreadCumb";
 const Checkout = () => {
   return (
-		<div className="wrapper">
-			<div className="container">
-				<CheckoutForm/>
-				<CartDetails/>
-			</div>
-		</div>
+    <div className="wrapper">
+      <BreadCumb data={"Checkout"} />
+      <div className={`container ${checkoutStyle.checkoutContainer}`}>
+        <CheckoutForm />
+        <CartDetails />
+      </div>
+    </div>
   );
 };
 
--- src/pages/customer/checkout/components/checkoutCom/CheckoutForm.jsx
+++ src/pages/customer/checkout/components/checkoutCom/CheckoutForm.jsx
@@ -1,31 +1,87 @@
-import React from "react";
-
+import { NavLink } from "react-router-dom";
+import checkoutStyle from "./checkout.module.css";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 const CheckoutForm = () => {
   return (
-		<div className=''>
-			{/* Billing details */}
-			<div className="">
-				<label htmlFor='name'> Name : </label>
-				<input type='text' placeholder='Enter your Name' id='name'/>
-			</div>
-			<div className='d-flex'>
-				<label htmlFor='email'> Email : </label>
-				<input type='email' placeholder='Enter your Email' id='email'/>
-				<label htmlFor='phone'> Phone : </label>
-				<input type='email' placeholder='Enter your Email' id='phone'/>
-			</div>
-			{/* Payment method */}
-			<ul>
-				<li><input type='radio'/><span>Cache on delivery (COD)</span></li>
-				<li><input type='radio'/><span>Stripe</span></li>
-				<li><input type='radio'/><span>Paypal</span></li>
-			</ul>
-			{/* Order Notes */}
-			<div className="">
-				<label htmlFor='order_notes'> Order Notes : </label>
-				<textarea rows='5' cols='5' placeholder='Notes about your order' id='order_notes'></textarea>
-			</div>
-		</div>
+    <div className="">
+      {/* Billing details */}
+      <div className={checkoutStyle.name}>
+        {/* <label htmlFor="name"> Name : </label> */}
+        <input type="text" placeholder="Enter your Name" id="name" />
+      </div>
+      <div className={checkoutStyle.emailPhone}>
+        <div className={checkoutStyle.email}>
+          {/* <label htmlFor="email"> Email : </label> */}
+          <input type="email" placeholder="Enter your Email" id="email" />
+        </div>
+        <div className={checkoutStyle.phone}>
+          {/* <label htmlFor="phone"> Phone : </label> */}
+          <input type="number" placeholder="Phone" id="phone" />
+        </div>
+      </div>
+      {/* Payment method */}
+
+      <div className="my-3">
+        <h4 className="mt-4">Payment Method</h4>
+        <div class="form-check">
+          <input
+            class="form-check-input my-2"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+          />
+          <label class="form-check-label ms-2 my-2" for="flexRadioDefault1">
+            Stripe
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input my-2"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+            checked
+          />
+          <label class="form-check-label ms-2 my-2" for="flexRadioDefault2">
+            Cache on delivery (COD)
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input my-2"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault3"
+          />
+          <label class="form-check-label ms-2 my-2" for="flexRadioDefault3">
+            Paypal
+          </label>
+        </div>
+      </div>
+
+      {/* Order Notes */}
+      <div className="">
+        <label htmlFor="order_notes"> Order Notes : </label>
+        <textarea
+          rows="5"
+          cols="5"
+          placeholder="Notes about your order"
+          id="order_notes"
+        ></textarea>
+      </div>
+
+      <div className={checkoutStyle.submitBtn}>
+        <div>
+          <NavLink to="/">
+            <AiOutlineArrowLeft /> Back to cart
+          </NavLink>
+        </div>
+
+        <div className="d-flex justify-content-end">
+          <button className="btn btn-primary">Checkout</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
