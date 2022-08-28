14aa36ff0607d7e56fe5ceab274c195118d251c9
nitu-xs <42024930+nitu456@users.noreply.github.com>
2022-08-28 21:57:27 +0600

    checkout page added

--- src/pages/customer/checkout/components/checkoutCom/CartDetails.jsx
+++ src/pages/customer/checkout/components/checkoutCom/CartDetails.jsx
@@ -1,18 +1,29 @@
 import CartTotal from "../../../cart/components/cartCom/CartTotal";
+import Coupon from "../../../cart/components/cartCom/Coupon";
+import ProductDetails from "./ProductDetails";
 
 const CartDetails = () => {
   return (
   <div>
 		<h3>Cart Total</h3>
 		<div className="marketplace-products">
+			{/* Store wise total */}
 			<div className="">
 				<div><img src="#"/><span>Store Name</span></div>
+				<ProductDetails/>
 				<CartTotal/>
 			</div>
 			<div className="">
 				<div><img src="#"/><span>Store Name</span></div>
 				<CartTotal/>
 			</div>
+			{/* Coupon */}
+			<div className="">
+				<div>You have a coupon code?</div>
+				<Coupon/>
+			</div>
+			{/* Grand total */}
+			<CartTotal/>
 		</div>
   </div>
   );
