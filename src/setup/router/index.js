fd4067c7bce923a52d8175f5146c8523e0324fd5
nitu-xs <42024930+nitu456@users.noreply.github.com>
2022-08-29 12:37:28 +0600

    customer login

--- src/setup/router/index.js
+++ src/setup/router/index.js
@@ -34,6 +34,7 @@ import Checkout from "../../pages/customer/checkout/Checkout";
 import Support from "../../pages/customer/support/Support";
 import SellerRequest from "../../pages/customer/sellerRequest/SellerRequest";
 import SellerShop from "../../pages/customer/sellerShop/SellerShop";
+import CustomerLogin from "../../pages/customer/customerLogin/CustomerLogin";
 
 const Router = () => {
   return (
@@ -90,6 +91,8 @@ const Router = () => {
         <Route path="support" element={<Support />} />
         <Route path="seller-request" element={<SellerRequest />} />
         <Route path="seller-shop" element={<SellerShop />} />
+        {/* Customer Login */}
+        <Route path="login" element={<CustomerLogin/>} />
       </Route>
 
       <Route path="/*" element={<NotFound />} />
