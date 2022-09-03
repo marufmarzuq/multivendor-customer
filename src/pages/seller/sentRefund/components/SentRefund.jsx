0a9ed138e2f7f2a3c483e83595da5f0ca8310ab6
nitu-xs <62047110+nitu-xs@users.noreply.github.com>
2022-09-03 18:40:48 +0600

    update

--- src/pages/seller/sentRefund/components/SentRefund.jsx
+++ src/pages/seller/sentRefund/components/SentRefund.jsx
@@ -10,33 +10,6 @@ const SentRefund = () => {
         <section>
           <h5 className="px-md-4 pt-3 px-3 py-2">Applied Refund Request</h5>
         </section>
-
-        {/* <section>
-          <div
-            className={`row px-0 mx-0 ps-3 mt-4 pt-2 ${sentRefundStyle.header}`}
-          >
-            <div className={`col-4 col-lg-2 ${sentRefundStyle.hide}`}>
-              <p>#</p>
-            </div>
-            <div className={`col-4 col-lg-2 ${sentRefundStyle.hide}`}>
-              <p>Date</p>
-            </div>
-            <div className="col-4 col-lg-2">
-              <p>Order Id</p>
-            </div>
-            <div className={`col-4 col-lg-2 ${sentRefundStyle.hide}`}>
-              <p>Product</p>
-            </div>
-            <div className={`col-4 col-lg-2 `}>
-              <p> Amount</p>
-            </div>
-            <div className="col-4 col-lg-2">
-              <p>Status</p>
-            </div>
-          </div>
-          <hr />
-        </section> */}
-
         <section className={`px-4 ${sentRefundStyle.tableData}`}>
           <Table borderless responsive>
             <thead>
@@ -59,7 +32,7 @@ const SentRefund = () => {
                 <th className="text-center">
                   <small>Status</small>
                 </th>
-                {/* <th>Reason</th> */}
+                <th>Reason</th>
               </tr>
             </thead>
 
@@ -110,6 +83,7 @@ const SentRefund = () => {
                     <span className={sentRefundStyle.unpaid}>Rjected</span>
                   </small>
                 </td>
+                <td> Bad product</td>
               </tr>
             </tbody>
           </Table>
