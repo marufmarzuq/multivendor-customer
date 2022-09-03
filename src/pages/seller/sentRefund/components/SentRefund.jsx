54dad0a8773dfecb539c2893a0658f786fe328bd
nitu-xs <62047110+nitu-xs@users.noreply.github.com>
2022-09-03 20:36:39 +0600

    refunds added

--- src/pages/seller/sentRefund/components/SentRefund.jsx
+++ src/pages/seller/sentRefund/components/SentRefund.jsx
@@ -1,117 +1,83 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useEffect } from "react";
 import { Table } from "react-bootstrap";
-
 import sentRefundStyle from "./sentRefund.module.css";
+import { useSelector } from "react-redux";
+import { getApi } from "../../../../api/apiCall";
+import { setSentRefunds } from "../../../../redux/slices/seller/refunds";
 
 const SentRefund = () => {
+  const { sentRefunds , loading, error } = useSelector((state) => state.sentRefundsSlice);
+
+  useEffect(() => {
+    getApi("sentRefunds.json", setSentRefunds);
+    // getApi("v1/seller/sent-refunds/search=''", setSentRefunds);
+  }, []);
+
   return (
     <Fragment>
       <div className={`${sentRefundStyle.background}`}>
         <section>
           <h5 className="px-md-4 pt-3 px-3 py-2">Applied Refund Request</h5>
         </section>
-
-        {/* <section>
-          <div
-            className={`row px-0 mx-0 ps-3 mt-4 pt-2 ${sentRefundStyle.header}`}
-          >
-            <div className={`col-4 col-lg-2 ${sentRefundStyle.hide}`}>
-              <p>#</p>
-            </div>
-            <div className={`col-4 col-lg-2 ${sentRefundStyle.hide}`}>
-              <p>Date</p>
-            </div>
-            <div className="col-4 col-lg-2">
-              <p>Order Id</p>
-            </div>
-            <div className={`col-4 col-lg-2 ${sentRefundStyle.hide}`}>
-              <p>Product</p>
-            </div>
-            <div className={`col-4 col-lg-2 `}>
-              <p> Amount</p>
-            </div>
-            <div className="col-4 col-lg-2">
-              <p>Status</p>
-            </div>
-          </div>
-          <hr />
-        </section> */}
-
         <section className={`px-4 ${sentRefundStyle.tableData}`}>
           <Table borderless responsive>
             <thead>
               <tr>
-                <th>
-                  <small>#</small>
-                </th>
-                <th>
-                  <small>Date</small>
-                </th>
-                <th>
-                  <small>Order Id</small>
-                </th>
-                <th>
-                  <small>Product</small>
-                </th>
-                <th className="text-end">
-                  <small>Amount</small>
-                </th>
-                <th className="text-center">
-                  <small>Status</small>
-                </th>
-                {/* <th>Reason</th> */}
+                <th><small>#</small></th>
+                <th><small>Date</small></th>
+                <th><small>Order Id</small></th>
+                <th><small>Product</small></th>
+                <th className="text-end"><small>Amount</small></th>
+                <th><small>Reason</small></th>
+								<th className="text-center"><small>Status</small></th>
               </tr>
             </thead>
-
-            <tbody>
-              <tr>
-                <td>
-                  <small>1</small>
-                </td>
-                <td>
-                  <small>01-08-2022</small>
-                </td>
-                <td>
-                  <small>20220801-23454823</small>
-                </td>
-                <td>
-                  <small>Seller product</small>
-                </td>
-                <td className="text-end">
-                  <small>৳100,000.000</small>
-                </td>
-                <td className="text-center">
-                  <small>
-                    <span className={sentRefundStyle.paid}>Paid</span>
-                    <span className={sentRefundStyle.paid}>Accepted</span>
-                  </small>
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <small>1</small>
-                </td>
-                <td>
-                  <small>01-08-2022</small>
-                </td>
-                <td>
-                  <small>20220801-23454823</small>
-                </td>
-                <td>
-                  <small>Seller product</small>
-                </td>
-                <td className="text-end">
-                  <small>৳100,000.000</small>
-                </td>
-                <td className="text-center">
-                  <small>
-                    <span className={sentRefundStyle.unpaid}>UnPaid</span>
-                    <span className={sentRefundStyle.unpaid}>Rjected</span>
-                  </small>
-                </td>
-              </tr>
-            </tbody>
+						{ error ? <h1>{error}</h1> : ""}
+						{loading ? ( <tbody><tr><td>Loading</td></tr></tbody> )
+						: (
+							<tbody>
+								{ sentRefunds.length > 0 &&
+									sentRefunds.map((item,key) => {
+										return (
+											<tr key={key}>
+												<td>
+													<small>{item.id}</small>
+												</td>
+												<td>
+													<small>{item.orderDate}</small>
+												</td>
+												<td>
+													<small>{item.orderCode}</small>
+												</td>
+												<td>
+												{ item.products.length > 0 &&
+															item.products.map((product,i) => {
+															return (
+																<div key={i}>
+																	<small>{product.name}</small>
+																</div>
+															)
+													})}
+												</td>
+												<td className="text-end">
+													<small>{item.amount}</small>
+												</td>
+												<td>{item.reason}</td>
+												<td className="text-center">
+												{ item.status.length > 0 &&
+															item.status.map((child,j) => {
+															return (
+																<small key={j}>
+																	<span className={sentRefundStyle.paid}>{child.name}</span>
+																</small>
+															)
+													})}
+												</td>
+											</tr>
+										)
+								})}
+							</tbody>
+            )}
           </Table>
         </section>
       </div>
