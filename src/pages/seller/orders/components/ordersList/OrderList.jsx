61a106f08d9ea25844db7e05036fa2f86e0a5cf8
mdmaruf43 <marufahmedcse76@gmail.com>
2022-09-09 10:02:53 +0600

    fix: login & paginatin daynaic also added search

--- src/pages/seller/orders/components/ordersList/OrderList.jsx
+++ src/pages/seller/orders/components/ordersList/OrderList.jsx
@@ -1,87 +1,104 @@
 import React, { useEffect, Fragment } from "react";
+import axios from 'axios';
 import { BsDownload, BsEyeFill } from "react-icons/bs";
 import { useState } from "react";
 import orderListStyle from "./orderList.module.css";
 import PdfModal from "../../../../../common/pdfModal/PdfModal";
 import OrderModal from "../orderModal/OrderModal";
-import Select from "react-select";
-import { useSelector } from "react-redux";
-import { getApi } from "../../../../../api/apiCall";
-import { setOrders } from "../../../../../redux/slices/seller/orders";
-import TablePagination from "../../../../../common/tablePagination/TablePagination";
 import ReactPaginate from 'react-paginate';
+import SimpleLoading from "../../../../../pages/SimpleLoading";
+import { API_URL } from "../../../services/Api/api";
+import authHeader from "../../../services/auth-header";
+import { useDebounce } from "../../../hooks/useDebounce";
 
-const OrderList = ({ orderData, loading }) => {
+const OrderList = () => {
   	const [show, setShow] 					= useState(false);
   	const [pdfShow, setPdfShow] 			= useState(false);
   	const [currentItems, setCurrentItems]  	= useState([]);
 	const [pageCount, setPageCount]         = useState(0);
-    const [itemOffset, setItemOffset]       = useState(0);
 	const [orderSearch, setOrderSearch]     = useState("");
-    const itemsPerPage                      = 6;
+	const [loading, setLoading] 			= useState(false);
+	const [total, setTotal] 				= useState(0);
+	const debouncedSearchTerm               = useDebounce(orderSearch, 500);
 
-	const handlePageClick = (event) => {
-        const newOffset = (event.selected * itemsPerPage) % orderData.length;
-        setItemOffset(newOffset);
-    };
+	let limit = 10;	
 
+    useEffect(() => {
+		setLoading(true);
+		axios.get(`${API_URL}/orders?page=1&per_page=${limit}`, {
+			headers: {
+			"Authorization": authHeader(),
+			}
+		}).then((response) => {
+			setLoading(false);
+			setCurrentItems(response?.data?.data);
+			setTotal(response?.data?.total);
+		})
+    }, [limit]);
 
 	useEffect(() => {
-		const endOffset = itemOffset + itemsPerPage;
-        setCurrentItems(orderData.slice(itemOffset, endOffset));
-        setPageCount(Math.ceil(orderData.length / itemsPerPage));
-	}, [orderData, itemOffset, setCurrentItems]);
+		setPageCount(Math.ceil(total/ limit));
+	}, [total, limit]);
+
+	const handlePageClick = async (data) => {
+		let currentPage = data.selected + 1;
+		axios.get(`${API_URL}/orders?page=${currentPage}&per_page=${limit}`, {
+			headers: {
+				"Authorization": authHeader(),
+			}
+		}).then((response) => {
+			setCurrentItems(response?.data?.data);
+		})
+	};
   
 	return (
 		<Fragment>
 		<div className={`${orderListStyle.background}`}>
 			<section>
-			<div className="table-top-header d-flex justify-content-between">
-          		<div className="table-title"><h5 className="px-md-4 px-3 pt-3 py-2">Orders</h5></div>
-				<div className="table-filters px-md-4 px-3 pt-3 py-2">
-					<input
-						type="text"
-						className="table-search-input"
-						placeholder="Search by customer name"
-						value={orderSearch}  
-                    	onChange={(e) => setOrderSearch(e.target.value)}
-					/>
+				<div className="table-top-header d-flex justify-content-between">
+					<div className="table-title"><h5 className="px-md-4 px-3 pt-3 py-2">Orders</h5></div>
+					<div className="table-filters px-md-4 px-3 pt-3 py-2">
+						<input
+							type="text"
+							className="table-search-input"
+							placeholder="Search by customer name"
+							value={orderSearch}  
+							onChange={(e) => setOrderSearch(e.target.value)}
+						/>
+					</div>
 				</div>
-			</div>
 			</section>
-
 			<section>
-
-			<div
-				className={` ${orderListStyle.orderRow} px-0 mx-0 ps-3 mt-3 ${orderListStyle.header}`}
-			>
-				<div>
-				<p>#</p>
+				<div
+					className={` ${orderListStyle.orderRow} px-0 mx-0 ps-3 mt-3 ${orderListStyle.header}`}
+				>
+					<div>
+					<p>#</p>
+					</div>
+					<div>
+					<p>Order Code</p>
+					</div>
+					<div className={` ${orderListStyle.hide}`}>
+					<p>Num. of Products</p>
+					</div>
+					<div className={` ${orderListStyle.hide}`}>
+					<p>Amount</p>
+					</div>
+					<div className={` ${orderListStyle.hide}`}>
+					<p>Customer</p>
+					</div>
+					<div className={` text-center ${orderListStyle.hide}`}>
+					<p>Delivery Status</p>
+					</div>
+					<div className="text-center">
+					<p> Payment Status</p>
+					</div>
+					<div className="text-center">
+					<p>Options</p>
+					</div>
 				</div>
-				<div>
-				<p>Order Code</p>
-				</div>
-				<div className={` ${orderListStyle.hide}`}>
-				<p>Num. of Products</p>
-				</div>
-				<div className={` ${orderListStyle.hide}`}>
-				<p>Amount</p>
-				</div>
-				<div className={` ${orderListStyle.hide}`}>
-				<p>Customer</p>
-				</div>
-				<div className={` text-center ${orderListStyle.hide}`}>
-				<p>Delivery Status</p>
-				</div>
-				<div className="text-center">
-				<p> Payment Status</p>
-				</div>
-				<div className="text-center">
-				<p>Options</p>
-				</div>
-			</div>
 			</section>
-			{ loading ? <h3>Loading...</h3>
+			{ loading ? <SimpleLoading />
 				: (
 					<Fragment>
 						<section>
@@ -138,6 +155,7 @@ const OrderList = ({ orderData, loading }) => {
 					nextLabel="Next 🡢"
 					onPageChange={handlePageClick}
 					pageRangeDisplayed={5}
+					marginPagesDisplayed={2}
 					pageCount={pageCount}
 					previousLabel="🡠 Previous"
 					containerClassName="pagination"
