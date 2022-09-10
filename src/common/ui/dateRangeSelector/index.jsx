346b16aba9a4654aa830eb6336fa088a1eaf7ae2
mdmaruf43 <marufahmedcse76@gmail.com>
2022-09-10 12:45:39 +0600

    fix: date-range-picker state issue fix

--- src/common/ui/dateRangeSelector/index.jsx
+++ src/common/ui/dateRangeSelector/index.jsx
@@ -4,11 +4,14 @@ import "react-dates/initialize";
 import { DateRangePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
 
-const DateRangeSelector = ({handleDatesChange}) => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+const DateRangeSelector = ({startDate , endDate, setStartDate, setEndDate }) => {
   const [focusedInput, setFocusedInput] = useState(null);
-
+  const handleDatesChange = ({ startDate, endDate }) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+    console.log(startDate);
+    console.log(endDate);
+  };
   return (
     <DateRangePicker
       startDate={startDate}
--- src/pages/seller/receivedRefund/components/ReceivedRefunds.jsx
+++ src/pages/seller/receivedRefund/components/ReceivedRefunds.jsx
@@ -15,8 +15,9 @@ const ReceivedRefunds = () => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [perPage, setPerPage] = useState(per_page);
 	const [search, setSearch]     = useState(null);
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+	const [startDate, setStartDate] = useState(null);
+	const [endDate, setEndDate] = useState(null);
+
   useEffect(() => {
 		// getApi(`refund-requests?search_value=${search}&sort_by=created_at&per_page=${perPage}&page=${currentPage}`, setSentRefunds);
 		getApi(`refund-requests?per_page=${perPage}`, setReceivedRefunds);
@@ -31,19 +32,14 @@ const ReceivedRefunds = () => {
 		{ value: '15', label: '15' },
 		{ value: '20', label: '20' }
 	]
-	const handleDatesChange = ({ startDate, endDate }) => {
-    setStartDate(startDate);
-    setEndDate(endDate);
-    console.log(startDate);
-    console.log(endDate);
-  };
+	
   return (
     <div>
       <div className={`${receivedRefundStyle.background}`}>
         <section>
           <h5 className="px-md-4 pt-3 px-3 py-2">Received Refund Request</h5>
 					<div>
-						<DateRangeSelector handleDatesChange={handleDatesChange}/>
+						<DateRangeSelector startDate={startDate} endDate={endDate} setStartDate={setStartDate} setEndDate={setEndDate}/>
 						<button className="table-filter-btn">
 							<FiFilter />
 						</button>
