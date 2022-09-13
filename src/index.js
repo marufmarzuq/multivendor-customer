be7625b7181fe1c86e87d77a671e826ebf40cc0b
mdmaruf43 <marufahmedcse76@gmail.com>
2022-09-13 21:38:47 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import "react-toastify/dist/ReactToastify.css";
 import "../src/styles/sass/style.scss";
 import AppProviders from "./setup/providers";
 import { ToastContainer } from "react-toastify";
-import { CookiesProvider } from "react-cookie";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
@@ -16,9 +15,7 @@ const root = ReactDOMClient.createRoot(container);
 root.render(
   <React.StrictMode>
     <AppProviders>
-      <CookiesProvider>
         <App />
-      </CookiesProvider>
       <ToastContainer />
     </AppProviders>
   </React.StrictMode>
