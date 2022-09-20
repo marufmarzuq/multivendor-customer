1e273ac1d9a13d24492875896eaa60ba6bf0a6cc
nitu-xs <62047110+nitu-xs@users.noreply.github.com>
2022-09-20 09:45:11 +0600

    add

--- src/pages/seller/dashboard/components/DashboardCom.jsx
+++ src/pages/seller/dashboard/components/DashboardCom.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import dashboardStyle from "./dashboard.module.css";
 import { BsCurrencyDollar, BsShop } from "react-icons/bs";
