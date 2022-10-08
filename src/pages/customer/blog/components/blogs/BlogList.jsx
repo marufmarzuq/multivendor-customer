7b99a48ecfed3c8fcbd697a9b4f3340ef06b4087
atik65 <atikhasan801@gmail.com>
2022-10-08 22:39:57 +0600

    category css and attribute css done and frontend pagination

--- src/pages/customer/blog/components/blogs/BlogList.jsx
+++ src/pages/customer/blog/components/blogs/BlogList.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import SimpleLoading from "../../../../../common/loading/SimpleLoading";
+import FrontendPagination from "../../../../../common/pagination/frontend/FrontendPagination";
 import PaginationCom from "../../../../../common/pagination/PaginationCom";
 import BlogCard from "./BlogCard";
 import blogsStyle from "./blogs.module.css";
@@ -7,30 +8,31 @@ import blogsStyle from "./blogs.module.css";
 const BlogList = (props) => {
   return (
     <>
-      { props.loading ? ( <SimpleLoading /> ) :
-				props.currentItems.length > 0 ? (
-					props.currentItems.map((item, key) => {
-							return (
-								<div className={blogsStyle.blogsContainer} key={key}>
-									<NavLink to={`/blog/${item.id}`}>
-										<BlogCard item={item}/>
-									</NavLink>
-								</div>
-								);
-							})
-						) :
-						(	<p className="text-center">There is no item</p>)
-			}
-			{props.currentItems.length > 0 && (
-          <PaginationCom
-            currentItem={props.currentItems}
-            perPage={props.perPage}
-            pageCount={props.pageCount}
-            currentPage={props.currentPage}
-            setPerPage={props.setPerPage}
-            setCurrentPage={props.setCurrentPage}
-          />
-        )}
+      {props.loading ? (
+        <SimpleLoading />
+      ) : props.currentItems.length > 0 ? (
+        props.currentItems.map((item, key) => {
+          return (
+            <div className={blogsStyle.blogsContainer} key={key}>
+              <NavLink to={`/blog/${item.id}`}>
+                <BlogCard item={item} />
+              </NavLink>
+            </div>
+          );
+        })
+      ) : (
+        <p className="text-center">There is no item</p>
+      )}
+      {props.currentItems.length > 0 && (
+        <FrontendPagination
+          currentItem={props.currentItems}
+          perPage={props.perPage}
+          pageCount={props.pageCount}
+          currentPage={props.currentPage}
+          setPerPage={props.setPerPage}
+          setCurrentPage={props.setCurrentPage}
+        />
+      )}
     </>
   );
 };
