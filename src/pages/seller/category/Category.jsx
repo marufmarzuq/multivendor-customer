571f0b8633fb775d4da73b2edbb2ab57e5dacb4b
atik65 <atikhasan801@gmail.com>
2022-10-08 22:51:02 +0600

    category css and attribute css done and frontend pagination

--- src/pages/seller/category/Category.jsx
+++ src/pages/seller/category/Category.jsx
@@ -4,14 +4,14 @@ import AddCategory from "./components/AddCategory";
 import CategoryTable from "./components/CategoryTable";
 
 const Category = () => {
-    return (
-			<div className={`background`}>
-				<div className="prod-grid-container">
-					<CategoryTable />
-					<AddCategory />
-				</div>
-			</div>
-    );
+  return (
+    <div className={`background`}>
+      <div className="prod-grid-container">
+        <CategoryTable />
+        <AddCategory />
+      </div>
+    </div>
+  );
 };
 
 export default Category;
