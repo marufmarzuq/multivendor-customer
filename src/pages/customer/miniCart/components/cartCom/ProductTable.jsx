aab2912ef3a865b1a45fb67d6502647ea1b2d716
atik65 <atikhasan801@gmail.com>
2022-10-15 20:29:54 +0600

    mini cart complete

--- src/pages/customer/miniCart/components/cartCom/ProductTable.jsx
+++ src/pages/customer/miniCart/components/cartCom/ProductTable.jsx
@@ -1,104 +1,94 @@
 import {
   AiOutlineCheckCircle,
   AiOutlineDelete,
+  AiOutlineMinusCircle,
+  AiOutlinePlusCircle,
   AiOutlineShoppingCart,
 } from "react-icons/ai";
+import { BiSad } from "react-icons/bi";
 import { NavLink } from "react-router-dom";
+import { useCart } from "react-use-cart";
 import CartTotal from "../../../cart/components/cartCom/CartTotal";
 import miniCartStyle from "../../miniCart.module.css";
 const ProductTable = () => {
+  const { isEmpty, totalUniqueItems, items, updateItemQuantity, removeItem } =
+    useCart();
+
   return (
     <>
       <div className="pt-3">
         <div className={miniCartStyle.miniCartContainer}>
-          <section>
-            <div className={miniCartStyle.miniCart}>
-              <div>
-                <img
-                  src="https://farmart.botble.com/storage/products/6-150x150.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={miniCartStyle.content}>
-                <h5> Nikon Camera </h5>
-                <h6> $ 100.00 </h6>
-                <small>( color: black )</small>
-              </div>
-              <div className="d-flex justify-content-end">
-                <button>
-                  <AiOutlineDelete />
-                </button>
-              </div>
-            </div>
-          </section>
+          {isEmpty ? (
+            <p className={miniCartStyle.noItem}>
+              Your Cart is Empty
+              <BiSad className="ms-2" />
+            </p>
+          ) : (
+            <div>
+              {items.map((item) => {
+                return (
+                  <section>
+                    <div className={miniCartStyle.miniCart}>
+                      <div>
+                        <img
+                          src="https://farmart.botble.com/storage/products/6-150x150.jpg"
+                          alt=""
+                        />
+                      </div>
+                      <div className={miniCartStyle.content}>
+                        <h5>{item.name}</h5>
+                        <h5>(x {item.quantity})</h5>
+                        <h6> $ {item.price} </h6>
+                        <small>( color: black )</small>
+                      </div>
+                      <div className={miniCartStyle.cartIncreaseButton}>
+                        <button
+                          onClick={() =>
+                            updateItemQuantity(item.id, item.quantity - 1)
+                          }
+                        >
+                          <AiOutlineMinusCircle />
+                        </button>
+                        <button
+                          onClick={() =>
+                            updateItemQuantity(item.id, item.quantity + 1)
+                          }
+                        >
+                          <AiOutlinePlusCircle />
+                        </button>
 
-          <section>
-            <div className={miniCartStyle.miniCart}>
-              <div>
-                <img
-                  src="https://farmart.botble.com/storage/products/6-150x150.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={miniCartStyle.content}>
-                <h5> Nikon Camera </h5>
-                <h6> $ 100.00 </h6>
-                <small>( color: black )</small>
-              </div>
-              <div className="d-flex justify-content-end">
-                <button>
-                  <AiOutlineDelete />
-                </button>
-              </div>
-            </div>
-          </section>
+                        <button onClick={() => removeItem(item.id)}>
+                          <AiOutlineDelete />
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                );
+              })}
+              <section>
+                <div className="mb-4">
+                  <CartTotal cartTotalTitle="checkout" />
+                </div>
+                <div className={miniCartStyle.buttons}>
+                  <div>
+                    <button className="btn btn-primary">
+                      <NavLink to="cart">
+                        <AiOutlineShoppingCart /> View Cart
+                      </NavLink>
+                    </button>
+                  </div>
 
-          <section>
-            <div className={miniCartStyle.miniCart}>
-              <div>
-                <img
-                  src="https://farmart.botble.com/storage/products/6-150x150.jpg"
-                  alt=""
-                />
-              </div>
-              <div className={miniCartStyle.content}>
-                <h5> Nikon Camera </h5>
-                <h6> $ 100.00 </h6>
-                <small>( color: black )</small>
-              </div>
-              <div className="d-flex justify-content-end">
-                <button>
-                  <AiOutlineDelete />
-                </button>
-              </div>
+                  <div className="d-flex justify-content-end">
+                    <button className="btn btn-primary">
+                      <NavLink to="/checkout">
+                        <AiOutlineCheckCircle /> Checkout
+                      </NavLink>
+                    </button>
+                  </div>
+                </div>
+              </section>
             </div>
-          </section>
-
-          <section>
-            <div className="mb-4">
-              <CartTotal cartTotalTitle="checkout" />
-            </div>
-            <div className={miniCartStyle.buttons}>
-              <div>
-                {" "}
-                <button className="btn btn-primary">
-                  <NavLink to="cart">
-                    {" "}
-                    <AiOutlineShoppingCart /> View Cart
-                  </NavLink>
-                </button>
-              </div>
-
-              <div className="d-flex justify-content-end">
-                <button className="btn btn-primary">
-                  <NavLink to="/checkout">
-                    {" "}
-                    <AiOutlineCheckCircle /> Checkout
-                  </NavLink>
-                </button>
-              </div>
-            </div>
-          </section>
+          )}
         </div>
       </div>
     </>
--- src/setup/providers/index.js
+++ src/setup/providers/index.js
@@ -2,11 +2,21 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../../redux/store/store";
+import { CartProvider } from "react-use-cart";
+import { toast } from "react-toastify";
 
 const AppProviders = ({ children }) => {
   return (
     <Provider store={store}>
-      <BrowserRouter>{children}</BrowserRouter>
+      <BrowserRouter>
+        <CartProvider
+          // onItemUpdate={() => toast.warning("Product already in cart")}
+          onItemAdd={() => toast.success("Product added to cart")}
+          onItemRemove={() => toast.success("Product removed from cart")}
+        >
+          {children}
+        </CartProvider>
+      </BrowserRouter>
     </Provider>
   );
 };
