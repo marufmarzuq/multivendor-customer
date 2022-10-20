e1a0f59b57a3253a2daf5aa2adb1e71fb3bc587f
nitu-xs <62047110+nitu-xs@users.noreply.github.com>
2022-10-20 22:56:01 +0600

    add

--- src/pages/customer/homepage/components/category/pinnedCategories/pinnedCategories.jsx
+++ src/pages/customer/homepage/components/category/pinnedCategories/pinnedCategories.jsx
@@ -4,6 +4,14 @@ import "../category.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {
+  catSlider1,
+  beauty,
+  clothing,
+  groceries,
+  bagAndShoes,
+} from "../../../../../../assets";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const PinnedCategories = ({pinnedCategories}) => {
   const settings = {
@@ -44,31 +52,102 @@ const PinnedCategories = ({pinnedCategories}) => {
   };
   return (
 		<div
-			style={{
-				overflowX: "hidden",
-				width: "100%",
-			}}
-			className="row mx-0 px-0"
-		>
-			<div className="col-12 col-xs-12 col-md-12  ">
-				<div className="mb-5">
-						<Slider {...settings}>
+		style={{
+			overflowX: "hidden",
+			width: "100%",
+		}}
+		className="row mx-0 px-0"
+	>
+		<div className="col-12 col-xs-12 col-md-12 mb-5">
+			<Slider {...settings}>
+				{
+				pinnedCategories.length == 0 ? (
+					<SkeletonTheme height={50}>
+					<p>
+						<Skeleton count={5} />
+					</p>
+				</SkeletonTheme>
+				) :
+				(
+					// pinnedCategories.map((category,key)=>{
+					// 	return(
+					// 		<div className="mini-slide" key={key}>
+					// 			<a href="#">
+					// 				<div className="image">
+					// 					<img src={category.banner} alt={category.name} />
+					// 				</div>
+					// 				<p>{category.name}</p>
+					// 			</a>
+					// 		</div>
+					// 	)
+					// })
+					<Slider {...settings}>
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={beauty} alt="" />
+							</div>
+							<p>Beauty</p>
+						</a>
+					</div>
 
-						{ pinnedCategories.map((category, index) => {
-								return (
-									<div className="mini-slide" key={index}>
-										<NavLink to={'#'}>
-											<div className="image">
-												<img src={category.banner} alt={category.name} />
-											</div>
-											<p>{category.name}</p>
-										</NavLink>
-									</div>
-								);
-							})}
-						</Slider>
-				</div>
-			</div>
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={clothing} alt="" />
+							</div>
+							<p>Clothing</p>
+						</a>
+					</div>
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={beauty} alt="" />
+							</div>
+							<p>Beauty</p>
+						</a>
+					</div>
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={bagAndShoes} alt="" />
+							</div>
+							<p>Bag and Shoes</p>
+						</a>
+					</div>
+
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={groceries} alt="" />
+							</div>
+							<p>Groceries</p>
+						</a>
+					</div>
+
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={clothing} alt="" />
+							</div>
+							<p>Clothing</p>
+						</a>
+					</div>
+
+					<div className="mini-slide">
+						<a href="#">
+							<div className="image">
+								<img src={bagAndShoes} alt="" />
+							</div>
+							<p>Bag and Shoes</p>
+						</a>
+					</div>
+				</Slider>
+				)
+				}
+
+			</Slider>
+		</div>
 	</div>
   );
 };
