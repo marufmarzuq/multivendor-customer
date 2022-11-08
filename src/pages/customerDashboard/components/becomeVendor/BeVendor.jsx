5ef2cea95453932ec91b0333f1a664a8a0f374c0
Farzana Akter Nitu <nitu@Farzanas-MacBook-Air.local>
2022-11-08 17:40:31 +0600

    build added

--- src/pages/customerDashboard/components/becomeVendor/BeVendor.jsx
+++ src/pages/customerDashboard/components/becomeVendor/BeVendor.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { FaRegEdit } from "react-icons/fa";
 import sellerRequestStyle from "./beVenodor.module.css";
-import { yupResolver } from "@hookform/resolvers/yup";
+import { FocusError } from "focus-formik-error";
 import * as yup from "yup";
 import { toast } from "react-toastify";
-import axios from "axios";
+import { useFormik } from "formik";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { saveToLocalStorage } from "../../../../utils/seller/manageLocalStorage";
 import { markutosFrontendApi } from "../../../services/Api/api";
 import customerAuthHeader from "../../../services/customer-auth-header";
 
@@ -26,138 +23,170 @@ const schema = yup.object().shape({
 
 const BeVendor = () => {
   const [loading, setLoading] = useState(false);
-  const notify = (text) => toast(text);
-  const navigate = useNavigate();
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
+  const formik = useFormik({
+    validationSchema: schema,
+    initialValues: {
+      first_name: "",
+      last_name: "",
+      email: "",
+      shop_address: "",
+      shop_name: "",
+    },
+    enableReinitialize: true,
+    onSubmit: (values, action) => {
+
+	setLoading(true);
+	markutosFrontendApi
+	.post(`/dashboard/become-a-vendor/post`, values , 
+		{
+			headers: {
+				Authorization: customerAuthHeader(),
+			},
+		}
+	)
+	.then((res) => {
+		setLoading(false);
+		toast.success(res.data.message);
+		action.resetForm();
+	})
+	.catch((e) => {
+		toast.error(e.message);
+		setLoading(false);
+	});
+    },
   });
 
-  const submitForm = (data) => {
-    setLoading(true);
-    axios
-      .post(`${markutosFrontendApi}/dashboard/become-a-vendor/post`, data , {
-		headers: {
-            Authorization: customerAuthHeader(),
-          },
-      })
-      .then((response) => {
-        saveToLocalStorage(response?.data);
-        setLoading(false);
-        notify(response?.data?.message);
-        navigate("/seller");
-      })
-      .catch((error) => {
-        setLoading(false);
-        notify(error?.response?.data?.email[0]);
-      });
-  };
+  const {
+    values,
+    handleChange,
+    touched,
+    errors,
+    handleSubmit,
+    handleBlur,
+  } = formik;
+
   return (
-    <form onSubmit={handleSubmit(submitForm)}>
-      <div className={sellerRequestStyle.authContainer}>
-        <div className={sellerRequestStyle.auth}>
-          <h4 className="text-center mb-3">Be a Seller</h4>
-          <hr className="mb-4" />
-          <div>
-            <label htmlFor="first_name">First Name</label>
-            <input
-              type="text"
-              id="first_name"
-              {...register("first_name", { required: true })}
-              placeholder="Enter your name"
-            />
-            {errors.first_name && (
-              <p className="error"> {errors.first_name?.message} </p>
-            )}
-          </div>
-          <div>
-            <label htmlFor="last_name">Last Name</label>
-            <input
-              type="text"
-              id="last_name"
-              {...register("last_name", { required: true })}
-              placeholder="Enter your name"
-            />
-            {errors.last_name && (
-              <p className="error"> {errors.last_name?.message} </p>
-            )}
-          </div>
-          <div>
-            <label htmlFor="vendor_email">Email</label>
-            <input
-              type="email"
-              id="vendor_email"
-              {...register("email", { required: true })}
-              placeholder="Enter your email"
-            />
-            {errors.email && <p className="error"> {errors.email?.message} </p>}
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              {...register("password", { required: true })}
-              placeholder="Enter your password"
-            />
-            {errors.password && (
-              <p className="error"> {errors.password?.message} </p>
-            )}
-          </div>
-          <div>
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input
-              type="password"
-              id="confirm-password"
-              {...register("confirm_password", { required: true })}
-              placeholder="Password Confirmation"
-            />
-            {errors.confirm_password && (
-              <p className="error"> {errors.confirm_password?.message} </p>
-            )}
-          </div>
-          <div>
-            <label htmlFor="shop-name">Shop Name</label>
-            <input
-              type="text"
-              id="shop-name"
-              {...register("shop_name", { required: true })}
-              placeholder="Enter your shop name"
-            />
-            {errors.shop_name && (
-              <p className="error"> {errors.shop_name?.message} </p>
-            )}
-          </div>
-          <div>
-            <label htmlFor="shop-url">Shop Addres</label>
-            <input
-              type="text"
-              id="shop-url"
-              {...register("shop_address", { required: true })}
-              placeholder="Enter your shop address"
-            />
-            {errors.shop_address && (
-              <p className="error"> {errors.shop_address?.message} </p>
-            )}
-          </div>
-          <div>
-            <button
-              className="btn btn-primary"
-              type="submit"
-              disabled={loading}
-            >
-              {loading && (
-                <span className="spinner-grow spinner-grow-sm"></span>
-              )}
-              <FaRegEdit /> Register
-            </button>
-          </div>
-        </div>
-      </div>
+    <form onSubmit={(e) => e.preventDefault()}>
+		<FocusError formik={formik} />
+		<div className={sellerRequestStyle.authContainer}>
+		<div className={sellerRequestStyle.auth}>
+			<h4 className="text-center mb-3">Be a Seller</h4>
+			<hr className="mb-4" />
+			<div>
+			<label htmlFor="first_name">First Name</label>
+			<input
+				type="text"
+				id="first_name"
+				name="first_name"
+                value={values.first_name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+				placeholder="Enter your first name"
+			/>
+			{errors.first_name && touched.first_name && (
+				<p className="error"> {errors.first_name} </p>
+			)}
+			</div>
+			<div>
+			<label htmlFor="last_name">Last Name</label>
+			<input
+				type="text"
+				id="last_name"
+				name="last_name"
+                value={values.last_name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+				placeholder="Enter your last name"
+			/>
+			{errors.last_name && touched.last_name && (
+				<p className="error"> {errors.last_name} </p>
+			)}
+			</div>
+			<div>
+			<label htmlFor="vendor_email">Email</label>
+			<input
+				type="email"
+				name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+				placeholder="Enter your email"
+			/>
+			{errors.email &&  touched.email && <p className="error"> {errors.email} </p>}
+			</div>
+			<div>
+			<label htmlFor="password">Password</label>
+			<input
+				type="password"
+				name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+				placeholder="Enter your password"
+			/>
+			{errors.password && touched.password &&  (
+				<p className="error"> {errors.password} </p>
+			)}
+			</div>
+			<div>
+			<label htmlFor="confirm-password">Confirm Password</label>
+			<input
+				type="password"
+				id="confirm-password"
+				name="confirm_password"
+                value={values.confirm_password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+				placeholder="Password Confirmation"
+			/>
+			{errors.confirm_password && touched.confirm_password && (
+				<p className="error"> {errors.confirm_password} </p>
+			)}
+			</div>
+			<div>
+			<label htmlFor="shop-name">Shop Name</label>
+			<input
+				type="text"
+				id="shop-name"
+				name="shop_name"
+                value={values.shop_name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+				placeholder="Enter your shop name"
+			/>
+			{errors.shop_name && touched.shop_name && (
+				<p className="error"> {errors.shop_name} </p>
+			)}
+			</div>
+			<div>
+			<label htmlFor="shop-url">Shop Address</label>
+			<textarea 
+			id="shop-url"
+			onBlur={handleBlur}
+			onChange={handleChange}
+			value={values.shop_address}
+			placeholder="Enter your shop address"
+			name="shop_address" rows={5} cols={5}></textarea>
+			{errors.shop_address && touched.shop_address &&  (
+				<p className="error"> {errors.shop_address} </p>
+			)}
+			</div>
+			<div>
+			<button
+			    onClick={handleSubmit}
+				className="btn btn-primary"
+				type="submit"
+				disabled={loading}
+			>
+				{loading && (
+				<span className="spinner-grow spinner-grow-sm"></span>
+				)}
+				<FaRegEdit /> Register
+			</button>
+			</div>
+		</div>
+		</div>
     </form>
   );
 };
