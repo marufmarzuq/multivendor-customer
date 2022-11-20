f9fb09edc3b268f3a8d73b37723160c33675a38f
marufmarzuq <marufmarzuq18@gmail.com>
2022-11-21 01:08:13 +0600

    support ticket css issue solved

--- src/pages/customerDashboard/components/supports/components/supportModal/components/Conversations.jsx
+++ src/pages/customerDashboard/components/supports/components/supportModal/components/Conversations.jsx
@@ -1,36 +1,47 @@
-
 import { AiOutlineUser } from "react-icons/ai";
 
-const Conversations = ({ replies , ticketId , createdAt }) => {
+const Conversations = ({ replies, ticketId, createdAt }) => {
   return (
     <>
-		<div className="widget-title" style={{ margin: "0 0 20px" }}>Conversations</div>
-		{
-			replies?.map((reply,key)=>{
-				return(
-				<div className="support-ticket-conversation-container" key={key+1}>
-					<div className={ key % 2 == 0 ? "stc-admin-container" : "stc-user-container"}>
-						<div className="support-replay-profile">
-						<div className="stc-user-img">
-							<AiOutlineUser />
-						</div>
-						<div className="stc-info">
-							<span>{reply.replied_by}</span>
-							<span className="stc-date">{reply.created_at}</span>
-						</div>
-						</div>
-					</div>
-					<div className="stc-message">
-						{reply.message}
-					</div>
-				</div>
-				)
-			})
-		}
+      <div className="widget-title" style={{ margin: "0 0 20px" }}>
+        Conversations
+      </div>
+      {replies?.map((reply, key) => {
+        return (
+          <div className="support-ticket-conversation-container" key={key + 1}>
+            {key % 2 === 0 ? (
+              <div className="stc-admin-container">
+                <div className="support-replay-profile">
+                  <div className="stc-info admin">
+                    <span>{reply.replied_by}</span>
+                    <span className="stc-date">{reply.created_at}</span>
+                  </div>
+                  <div className="stc-user-img">
+                    <AiOutlineUser />
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="stc-user-container">
+                <div className="support-replay-profile">
+                  <div className="stc-user-img">
+                    <AiOutlineUser />
+                  </div>
+                  <div className="stc-info">
+                    <span>{reply.replied_by}</span>
+                    <span className="stc-date">{reply.created_at}</span>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className="stc-message">{reply.message}</div>
+          </div>
+        );
+      })}
 
-		<div className="support-ticket-creating-date">
-			Ticket #{ticketId} has been created at { createdAt }
-		</div>
+      <div className="support-ticket-creating-date">
+        Ticket #{ticketId} has been created at {createdAt}
+      </div>
     </>
   );
 };
