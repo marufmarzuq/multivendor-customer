1b62fdfe06cb48861ab0792497a38b796cfa8c3b
marufmarzuq <marufmarzuq18@gmail.com>
2022-12-09 11:07:39 +0600

    Fixes

--- src/common/quickView/QuickView.jsx
+++ src/common/quickView/QuickView.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { useDispatch } from "react-redux";
 import "./quickView.css";
 import { BsChevronRight, BsStar, BsStarFill } from "react-icons/bs";
