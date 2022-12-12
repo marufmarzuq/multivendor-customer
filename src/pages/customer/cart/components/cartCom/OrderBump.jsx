21b5f284abbfee6810348286beeaf60dffe3bbe7
marufmarzuq <marufmarzuq18@gmail.com>
2022-12-12 11:39:59 +0600

    Cart page prod updated

--- src/pages/customer/cart/components/cartCom/OrderBump.jsx
+++ src/pages/customer/cart/components/cartCom/OrderBump.jsx
@@ -1,53 +1,44 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import GridOne from "../../../../../common/product/layout/GridOne";
 import { useCart } from "react-use-cart";
 import "react-loading-skeleton/dist/skeleton.css";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { markutosFrontendApi } from "../../../../services/Api/api";
+import GridView from "../../../../../common/product/prodsLayout/GridView";
 
 const OrderBump = () => {
-	const [loading, setLoading] = useState(false);
-	const [products, setProducts] = useState([]);
-	const { addItem, onItemAdd } = useCart();
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const { addItem, onItemAdd } = useCart();
 
-	useEffect(() => {
-	  setLoading(true);
-	  markutosFrontendApi
-		.get(`/cart/you-can-also-buy`)
-		.then((response) => {
-		  setLoading(false);
-		  setProducts(response?.data);
-		  });
-	},[]);
+  useEffect(() => {
+    setLoading(true);
+    markutosFrontendApi.get(`/cart/you-can-also-buy`).then((response) => {
+      setLoading(false);
+      setProducts(response?.data);
+    });
+  }, []);
 
-	const addToCart = (product) => {
-		addItem(product);
-	};
+  const addToCart = (product) => {
+    addItem(product);
+  };
 
-	return (
-		<div className="order-bump">
-		<h2 className="my-4">You can also buy :</h2>
-		<div className="row products-wrap">
-			{ 
-				loading == false ?
-					products?.map((product, index) => {
-						return (
-							<GridOne
-								product={product}
-								key={index}
-								addToCart={addToCart}
-							/>
-						);
-				}) : 
-				(
-				<SkeletonTheme height={50}>
-					<p><Skeleton count={5} /></p>
-				</SkeletonTheme>
-				)
-			}
-		</div>
-		</div>
-	);
+  return (
+    <div className="order-bump">
+      <h2 className="my-4">You can also buy :</h2>
+      <div className="row products-wrap">
+        {loading === false ? (
+          <GridView products={products} addToCart={addToCart} />
+        ) : (
+          <SkeletonTheme height={50}>
+            <p>
+              <Skeleton count={5} />
+            </p>
+          </SkeletonTheme>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrderBump;
